3f9b73bf4b672220d5bb09011ac1b12b30bb0c30
Shamim Hossain <51192688+shamimINFO@users.noreply.github.com>
2020-09-18 21:32:29 +0600

    Complate Theme
    Full Complete Theme

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
+    
+    //STICKY MENU
+    
+    $(".js-service-section").waypoint(function(direction){
+        if(direction == "down"){
+            $("nav").addClass("sticky");  
+        } else {
+            $("nav").removeClass("sticky");
+        }
+    });
+    
+
+    
+    
+    //MIXITUP 
     var mixer = mixitup('.container');
 });
