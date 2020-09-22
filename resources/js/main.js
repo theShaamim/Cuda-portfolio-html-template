8a3253884214c3bf574663d6f2488f1ea3e11c8e
Shamim Hossain <51192688+shamimINFO@users.noreply.github.com>
2020-09-23 00:21:20 +0600

    Final Revesion
    Final Revesion

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,18 +1,1 @@
-$(document).ready(function(){
-    
-    //STICKY MENU
-    
-    $(".js-service-section").waypoint(function(direction){
-        if(direction == "down"){
-            $("nav").addClass("sticky");  
-        } else {
-            $("nav").removeClass("sticky");
-        }
-    });
-    
-
-    
-    
-    //MIXITUP 
-    var mixer = mixitup('.container');
-});
+function openNav(){document.getElementById("myNav").style.width="100%"}function closeNav(){document.getElementById("myNav").style.width="0%"}$(document).ready(function(){$(".js-service-section").waypoint(function(n){"down"==n?$("nav").addClass("sticky"):$("nav").removeClass("sticky")});mixitup(".container")});
